12573a6f60b59a18cf2a7ddf63aaa290a1a00cca
ILekawa9 <95458978+ILekawa9@users.noreply.github.com>
2022-04-14 18:31:49 -0400

    update

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -10,13 +10,8 @@ function createMap(response) {
         zoom: mapZoom
     });
 
-    L.titleLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        titleSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/light-v10",
-        accessToken: API_KEY 
+    L.titleLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(myMap);
 
     L.geoJSON(response, {
